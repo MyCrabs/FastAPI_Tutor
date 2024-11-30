15f13706e5b6a6261975f67906a4e18cf32b256e
Your Name <you@example.com>
2024-11-30 09:03:47 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+﻿from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
+class User(BaseModel):
+    id: int
+    name: str
+    
+@app.post("/home")
+async def home(user: User):
+    return {"message":"User be created successfully", "user":user}
